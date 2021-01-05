2a03b1b1863cc39dee98da7b373fa5808302f7ad
GitHub Action <action@github.com>
2021-01-05 09:38:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/germanslok/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/germanslok/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.germanslok.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.germanslok.testplugin.MyBundle
 
 class MyApplicationService {
 
